c5f64cad19fbd6f5ff8b3a4e64d05fc1b571a6bf
Pedro Henrique <pedrohenriquesnows22@gmail.com>
2025-05-24 00:53:14 -0300

    first commit

---
+++ src/main/java/com/example/fraudeZero/FraudeZeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.fraudeZero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudeZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudeZeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fraudeZero/FraudeZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fraudeZero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudeZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
