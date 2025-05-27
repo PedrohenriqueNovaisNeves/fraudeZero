c4589ae09a4f2bab1d148545a329000214b37891
Pedro Henrique <pedrohenriquesnows22@gmail.com>
2025-05-27 19:18:31 -0300

    user model class and bank account created and database credentials allocated in application.properties

--- src/main/java/com/example/fraudeZero/models/BankAccount.java
+++ src/main/java/com/example/fraudeZero/models/BankAccount.java
@@ -1,4 +1,54 @@
 package com.example.fraudeZero.models;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_BANKACCOUNT")
 public class BankAccount {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idBankAccount;
+    @Column(nullable = false, unique = true)
+    private String owner;
+    @Column(nullable = false)
+    private BigDecimal transferLimit;
+    @Column(nullable = false)
+    private BigDecimal bankBalance;
+
+    public UUID getIdBankAccount() {
+        return idBankAccount;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public BigDecimal getTransferLimit() {
+        return transferLimit;
+    }
+
+    public BigDecimal getBankBalance() {
+        return bankBalance;
+    }
+
+    public void setIdBankAccount(UUID idBankAccount) {
+        this.idBankAccount = idBankAccount;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void setTransferLimit(BigDecimal transferLimit) {
+        this.transferLimit = transferLimit;
+    }
+
+    public void setBankBalance(BigDecimal bankBalance) {
+        this.bankBalance = bankBalance;
+    }
 }
--- src/main/java/com/example/fraudeZero/models/User.java
+++ src/main/java/com/example/fraudeZero/models/User.java
@@ -1,4 +1,53 @@
 package com.example.fraudeZero.models;
 
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USER")
 public class User {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idUser;
+    private String nameUser;
+    @Column(nullable = false)
+    private String cpfUser;
+    @Column(nullable = false, unique = true)
+    private int accountNumber;
+
+    public UUID getIdUser() {
+        return idUser;
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getCpfUser() {
+        return cpfUser;
+    }
+
+    public void setIdUser(UUID idUser) {
+        this.idUser = idUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    public void setCpfUser(String cpfUser) {
+        this.cpfUser = cpfUser;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 }
